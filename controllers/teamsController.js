0f80534a6eb328407d2f7ce882bba0f32ecb8278
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-09-24 14:02:15 -0500

    finished Team routes

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -7,7 +7,6 @@ const PlayerModel = require('../models').Player;
 //get all Teams
 router.get('/', async (req, res) => {
     let teams = await TeamModel.findAll();
-    console.log(teams);
     res.json({ teams });
 });
 
@@ -15,7 +14,31 @@ router.get('/', async (req, res) => {
 router.get('/profile/:id', async (req, res) =>{
     let team = await TeamModel.findByPk(req.params.id);
     res.json(team);
+});
+
+//create a Team
+router.post('/', async (req, res) => {
+    let team = await TeamModel.create(req.body);
+    console.log('in post',team,req.body)
+    res.json({ team });
+});
+
+//update a team
+router.put('/:id', async (req, res) => {
+    let team = await TeamModel.update(req.body, {
+        where: { id: req.params.id },
+        returning: true,
+    });
+    res.json(team);
 })
 
-
+//delete a team
+router.delete('/:id', async (req, res) => {
+    let team = await TeamModel.destroy({
+        where: { id: req.params.id},
+    });
+    res.json({
+        message: `Team with id ${req.params.id} deleted`
+    })
+})
 module.exports = router;
